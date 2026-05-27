ec12a1df6902f5888ed46b4e5acbc1974534d338
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-05-27 13:38:08 +0530

    Print Patter

---
+++ Print_Pattern.java
@@ -0,0 +1,15 @@
+package Java_01;
+import java.lang.*;
+
+public class Print_Pattern {
+    public static void main(String[] args) {
+        System.out.println("There is your pattern.");
+        int i;
+        int j;
+        int k;
+        for(i=0; i<6; i++){
+            System.out.println(i);
+        }
+        
+    }
+}
