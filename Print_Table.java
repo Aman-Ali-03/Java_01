b2a668f1dc694a18d6467ea6b9fc1bb7bb542aa5
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-05-27 12:00:14 +0530

    Print Table Of Given Number

---
+++ Print_Table.java
@@ -0,0 +1,19 @@
+package Java_01;
+import java.util.Scanner;
+
+public class Print_Table {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter You Number To Print Table :");
+        int a = s.nextInt();
+        int b = 1;
+        while(b<=10){
+            int c= a*b;
+            System.out.print(a + "x" + b );
+            System.out.print("=" + c);
+            System.out.println(" ");
+            b++;
+
+        }
+    }
+}
