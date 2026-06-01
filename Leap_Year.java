dccde35828de7b242809f0dfebdf9095fdcd25c8
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-06-01 18:59:36 +0530

    Check Year is Leap Year or Not

---
+++ Leap_Year.java
@@ -0,0 +1,18 @@
+package Java_01;
+
+import java.util.Scanner;
+
+public class Leap_Year {
+    public static void main(String[] args) {
+        System.out.print("Enter Your Year :");
+        Scanner Scan = new Scanner(System.in);
+        short year = Scan.nextShort();
+        if (year % 4 == 0) {
+            if (year % 400 == 0 || year % 100 != 0) {
+                System.out.println(year + "is leap year.");
+            } else {
+                System.out.println(year + "is not a leap year.");
+            }
+        }
+    }
+}
