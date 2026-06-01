6f25e8bf97aaba025ce56cf49014dbc76525176f
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-06-01 19:00:37 +0530

    Check Enter Year Is Leap Year Or NOt

--- Leap_Year.java
+++ Leap_Year.java
@@ -10,9 +10,9 @@ public class Leap_Year {
         if (year % 4 == 0) {
             if (year % 400 == 0 || year % 100 != 0) {
                 System.out.println(year + "is leap year.");
-            } else {
-                System.out.println(year + "is not a leap year.");
             }
+        } else {
+            System.out.println(year + "is not a leap year.");
         }
     }
 }
