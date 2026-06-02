c5fc5782a4911d97ad638fcf208fb758839ec360
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-06-02 13:47:38 +0530

    Basic Calculator

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+package Java_01;
+import java.util.Scanner;
+
+public class Calculator{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your first number :");
+        short first = scan.nextShort();
+        System.out.print("Enter your second number :");
+        short second = scan.nextShort();
+        System.out.print("Enter your operator :");
+        char operator = scan.next().charAt(0);
+        switch(operator){
+            case '+':
+                System.out.println("Addition of your number : " + (first+second));
+                break;
+            case '-':
+                System.out.println("Subtract first-second :" + (first-second));
+                break;
+            case '*':
+                System.out.println("Multiplication of your number : " + (first*second));
+                break;
+            case '/':
+                System.out.println("Division first//second" + (first/second));
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+}
