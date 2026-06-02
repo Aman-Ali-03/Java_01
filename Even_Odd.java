a4588692abac807722e3e2620d70b462fc3e2b25
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-06-02 13:25:00 +0530

    Check Enter Number Is Even/Odd.

--- Even_Odd.java
+++ Even_Odd.java
@@ -1,5 +1,20 @@
 package Java_01;
 
+import java.util.Scanner;
 public class Even_Odd {
-    
+    public static void main(String[] args) {
+        System.out.println("Enter your number :");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        if(num%2==0){
+            System.out.println("Enter number is even.");
+        }
+        else if(num%2 != 0){
+            System.out.println("enter number is odd.");
+        }
+        else{
+            System.out.println("Enter number is zero.");
+        }
+
+    }
 }
