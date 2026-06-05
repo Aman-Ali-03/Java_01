b8401bc26801e97fbe12e8a34d12862bb6c17661
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-06-05 20:48:03 +0530

    Count Vowel Of String Enter By The User.

--- Count_Vowel.java
+++ Count_Vowel.java
@@ -1,5 +1,30 @@
 package Java_01;
 import java.util.Scanner;
+import java.lang.String;
 public class Count_Vowel {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = scan.nextLine();
+        int sum = 0;
+        for(int i = 0; i < str.length(); i++){
+            char ch= str.charAt(i);
+            if(ch=='a'){
+                sum++;
+            }
+            else if(ch=='e'){
+                sum++;
+            }
+            else if(ch == 'i'){
+                sum++;
+            }
+            else if(ch == 'o'){
+                sum++;
+            }
+            else if(ch=='u'){
+                sum++;
+            }
+        }
+        System.out.println("The total vowels in the string is: " + sum );
+    }
 }
