95612725e05844e59c8f493dbae6704cc6bf4530
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-06-06 23:41:36 +0530

    Enter digit

---
+++ Sum_of_Digit.java
@@ -0,0 +1,17 @@
+package Java_01;
+import java.util.Scanner;
+public class Sum_of_Digit {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scan.nextInt();
+        int sum = 0;
+        while(num > 0){
+            int rem = num % 10;
+            sum = sum + rem;
+            num = num / 10;
+        }
+        System.out.println("The sum of the digits is: " + sum);
+    }
+}
+
