2a98381dda7e9f0165b82a66a5f66bf4b0b8a1a4
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-06-07 20:51:29 +0530

    Check Vote Eligiblity.

---
+++ Vote_Eligiblity.java
@@ -0,0 +1,14 @@
+package Java_01;
+import java.util.Scanner;
+public class Vote_Eligiblity {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scan.nextInt();
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
