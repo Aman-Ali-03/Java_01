e6737d0c910f852a9df9ec6ac7edf561300ebcdd
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-06-10 23:57:40 +0530

    Modified

--- Day.java
+++ Day.java
@@ -1,7 +1,34 @@
 package Java_01;
 import java.util.Scanner;
 public class Day {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        System.out.println("Enter your number to get day name : ");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        switch (number){
+            case 1:
+                System.out.println("Day 1: Monday");
+                break;
+            case 2:
+                System.out.println("Day 2: Tuesday");
+                break;
+            case 3:
+                System.out.println("Day 3: Wednesday");
+                break;
+            case 4:
+                System.out.println("Day 4: Thursday");
+                break;
+            case 5:
+                System.out.println("Day 5: Friday");
+                break; 
+            case 6:
+                System.out.println("Day 6: Saturday");
+                break;
+            case 7:
+                System.out.println("Day 7: Sunday");
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
     }
 }
