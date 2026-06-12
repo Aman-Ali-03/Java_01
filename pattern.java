e0c50b89202720344b71b8e455c1545d00c8b4a4
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-06-12 17:46:31 +0530

    Print Pattern.

---
+++ pattern.java
@@ -0,0 +1,11 @@
+package Java_01;
+public class pattern {
+    public static void main(String[] args) {
+        for(short i=0 ; i<5 ; i++){
+            for(short j=0 ; j<5 ; j++){
+                System.out.print("# ");
+            }
+            System.out.println();
+        }
+    }
+}
