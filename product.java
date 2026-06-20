9b5797daa8730f2a443a1a1a159621ce8e79c220
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-06-20 22:29:11 +0530

    Clear Program With Extra Space.

--- product.java
+++ product.java
@@ -1,51 +1,40 @@
 package Java_01;
-
 class Produc {
     private String itemNo;
     private String name;
     private double price;
     private short qty;
-
     public Produc(String itemno) {
         itemNo = itemno;
     }
-
     public Produc(String itemno, String name) {
         itemNo = itemno;
         this.name = name;
     }
-
     public Produc(String itemno, String name, double price, short qty) {
         itemNo = itemno;
         this.name = name;
         setPrice(price);
         setQuantity(qty);
     }
-
     public String getItemNo() {
         return itemNo;
     }
-
     public String getName() {
         return name;
     }
-
     public double getPrice() {
         return price;
     }
-
     public short getQuantity() {
         return qty;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
-
     public void setQuantity(short qty) {
         this.qty = qty;
     }
-
     public void displayProduct() {
         System.out.println("Item No: " + itemNo);
         System.out.println("Name: " + name);
@@ -53,7 +42,6 @@ class Produc {
         System.out.println("Quantity: " + qty);
     }
 }
-
 public class product {
     public static void main(String[] args) {
         Produc p1 = new Produc("101");
