2294accfa5f7124da4f09fa6333c912b756d4045
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-06-23 20:00:50 +0530

    Correction.

--- ATM.java
+++ ATM.java
@@ -34,6 +34,9 @@ public class ATM {
                     break;
                 case 4:
                     System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid Choice.");
             }
 
         }
