bef20c1ce292e7053c1a8b7656c44cda954537c9
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-07-08 17:54:17 +0530

    Learning Wraper Class.

---
+++ Wrapper.java
@@ -0,0 +1,15 @@
+package Java_01;
+import java.lang.Object;
+public class Wrapper {
+    public static void main(String[] args){
+        Object o = new Object();
+
+        Integer I = new Integer(10);
+        Integer I2 =Integer.valueOf(90);
+        int m = 9090;
+        Integer I3 = new Integer(m);
+
+        float f2 = 9.0f;
+        double d = 9.0;
+    }
+}
