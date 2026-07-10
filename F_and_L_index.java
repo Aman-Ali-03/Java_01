7e24749bba15f99b573660b47fecdc05c1a4756c
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-07-11 00:13:07 +0530

    The current one.

--- F_and_L_index.java
+++ F_and_L_index.java
@@ -2,61 +2,42 @@ package Java_01;
 import java.util.Arrays;
 public class F_and_L_index {
     public static void main(String[] args) {
-        int[] a = {2, 3, 4, 7,7, 7, 7, 7, 8, 9};
-        int target = 12;
+        int[] a = {1};
+        int target = 1;
         int[] b = inde(a, target);
         System.out.println(Arrays.toString(b));
     }
 
     static int[] inde(int[] nums, int tar) {
-        int start = 0, end = nums.length - 1;
-        int[] a = new int[2];
-        int[] u = {-1,-1};
-        if(start>=end){
-            return u;
-        }
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (nums[mid] == tar) {
+        int[] a = {-1,-1};
+        int start = finding(nums,tar,true);
+        int end = finding(nums,tar,false);
+        a[0] = start;
+        a[1] = end;
+        return a;
+    }
+    static int finding(int[] a,int tar,boolean firstindex){
+        int start = 0;
+        int end = a.length-1;
+        int ans = -1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(a[mid]==tar){
+                ans = mid;
+                if(firstindex){
+                    end = mid -1;
+                }
+                else{
+                    start = mid + 1;
+                }
+            }
+            else if(a[mid]<tar){
                 start = mid + 1;
-                a[1]=mid;
-            } else if (nums[mid] < tar) {
-                start = mid + 1;
-            } else {
+            }
+            else{
                 end = mid - 1;
             }
         }
-        int e=nums.length-1,s=0;
-        while(s<=e)
-        {
-            int mid = (s+e)/2;
-            if(nums[mid]==tar)
-            {
-                if(nums[mid-1]==tar)
-                {
-                    e = mid - 1;
-                }else {
-                    a[0] = mid;
-                    break;
-                }
-            }
-            else if(nums[mid]<tar)
-            {
-                s = mid + 1;
-            }
-            else
-            {
-                e = mid -1;
-            }
-        }
-        if(a[0]!=0 || a[1]!=0){
-            return a;
-        }
-        else if(a[0]==0 || a[1]==0){
-            return a;
-        }
-        else{
-            return u;
-        }
+        return ans;
     }
 }
