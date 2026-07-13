a060890cc0b17642cd1cb3fcc894702b31a65e13
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-07-13 23:35:24 +0530

    Learning Bubble Short.

---
+++ Bubble_Short.java
@@ -0,0 +1,27 @@
+package Java_01;
+// BUBBLE SHORT 
+import java.util.Arrays;
+public class Bubble_Short
+{
+    public static void main(String[] atgs)
+    {
+        int[] a = {3,1,5,4,2,9,7,6,8};
+        shot(a);
+        System.out.println(Arrays.toString(a));
+    }
+    static void shot(int[] a)
+    {
+        int temp=0;
+        for(int i=a.length; i>0; i--)
+        {
+            for(int j=0; j<i-1; j++)
+            {
+                if(a[j]>a[j+1]){
+                    temp = a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+    }
+}
