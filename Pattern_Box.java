e82f1f79b62e41fe04daa4dcf7caa18d80386e25
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-07-19 20:13:27 +0530

    Enter Number Of Row And Print The Start Center Hollow Box Pattern.

---
+++ Pattern_Box.java
@@ -0,0 +1,34 @@
+package Java_01;
+import java.util.Scanner;
+public class Pattern_Box
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = in.nextInt();
+        for(int i=1; i<=n; i++)
+        {
+            if(i==1||i==n)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            else {
+                for(int j=1; j<=n; j++)
+                {
+                    if(j==1||j==n){
+                        System.out.print("* ");
+                    }
+                    else{
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
