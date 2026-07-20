b4047ff819686cdfb8dbc30e231e0ee0d233c440
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-07-20 20:00:15 +0530

    Print Diamond Pattern.

---
+++ Diamond.java
@@ -0,0 +1,36 @@
+package Java_01;
+
+public class Diamond
+{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            for (int l = 2; l <= i; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1; i<=5; i++)
+        {
+            for(int k=0; k<i; k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=5; j>i; j--)
+            {
+                System.out.print("*");
+            }
+            for(int j=4; j>i; j--)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
