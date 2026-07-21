0b09494a69806814155fd4935ff2fd14980f6918
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-07-21 22:52:31 +0530

    Solution of Leet Code Problem no. 1480.

---
+++ sum.java
@@ -0,0 +1,27 @@
+package Java_01;
+
+import java.util.Arrays;
+
+public class sum {
+    public static void main(String[] args) {
+        int[] a = { 1, 1, 1, 1, 1, 1, 1, 1 };
+        int[] n = sums(a);
+        System.out.println(Arrays.toString(n));
+    }
+
+    static int[] sums(int[] a) {
+        int[] n = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            n[i] = sum(a, i);
+        }
+        return n;
+    }
+
+    static int sum(int[] a, int i) {
+        int sum = 0;
+        for (int j = 0; j <= i; j++) {
+            sum += a[j];
+        }
+        return sum;
+    }
+}
